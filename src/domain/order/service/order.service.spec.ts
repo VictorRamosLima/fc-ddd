c2aa5fff00703db444c838435035a6216e7fb7ba
VictorRamosLima <viictorramosl@gmail.com>
2024-03-28 07:09:49 -0300

    initial commit

---
+++ src/domain/order/service/order.service.spec.ts
@@ -0,0 +1,27 @@
+import crypto from "crypto";
+
+import { OrderFactory } from "../factory/order.factory";
+
+describe("Order Factory unit test", () => {
+  it("should create an order", () => {
+    const orderProps = {
+      id: crypto.randomUUID(),
+      customerId: crypto.randomUUID(),
+      items: [
+        {
+          id: crypto.randomUUID(),
+          productId: crypto.randomUUID(),
+          name: "Item 1",
+          unitPrice: 100,
+          quantity: 3,
+        },
+      ],
+    };
+
+    const order = OrderFactory.create(orderProps);
+
+    expect(order.id).toBe(orderProps.id);
+    expect(order.customerId).toBe(orderProps.customerId);
+    expect(order.items.length).toBe(1);
+  });
+});
---
+++ src/domain/product/repository/productRepositoryInterface.ts
@@ -0,0 +1,4 @@
+import { RepositoryInterface } from "@domain/@shared/repository/repositoryInterface";
+import { Product } from "@domain/product/entity/product";
+
+export type ProductRepositoryInterface = RepositoryInterface<Product>
